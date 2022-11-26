13d36876de2be247018fd627162dde39635a7a50
Alex Castelo <alexcastelo437@gmail.com>
2022-11-26 01:12:38 -0300

    feat: created app file

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const router = require("./routes")
+require("./database/mongo/index")
+
+class App {
+    constructor() {
+        this.server = express()
+        this.middleware()
+        this.routes()
+    }
+
+    middleware() {
+        this.server.use(express.json())
+    }
+
+    routes() {
+        router(this.server)
+    }
+}
+
+module.exports = new App().server
