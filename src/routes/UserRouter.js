1338fa080868316de049595d420101789ab7b030
Alex Castelo <alexcastelo437@gmail.com>
2022-11-26 11:46:14 -0300

    feat: added user validation in post and put route

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,10 +1,11 @@
 const UserController = require("../app/controller/UserController")
-
+const userValidation = require("../app/validations/user/UserValidation")
+const userValidationUpdate = require("../app/validations/user/UserValidateUpdate")
 module.exports = (server, routes, prefix = "/users" ) => {
-    routes.post("/", UserController.create)
+    routes.post("/", userValidation , UserController.create)
     routes.get("/", UserController.list)
     routes.get("/:id", UserController.getById)
-    routes.put("/:id", UserController.update)
+    routes.put("/:id", userValidationUpdate ,UserController.update)
     routes.delete("/:id", UserController.delete)
 
     server.use(prefix,routes)
