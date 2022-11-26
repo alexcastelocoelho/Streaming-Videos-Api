bed0f2f86b9c02bf3b4a801985f0280e4cf2e54e
Alex Castelo <alexcastelo437@gmail.com>
2022-11-26 20:58:34 -0300

    feat: adding video routes in route file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const {Router} = require("express")
 const User = require("./UserRouter")
-
+const Video = require("./VideoRouter")
 module.exports = (server) => {
     server.use((req, res, next) => {
         User(server, new Router())
+        Video(server, new Router())
 
         next();
     })
