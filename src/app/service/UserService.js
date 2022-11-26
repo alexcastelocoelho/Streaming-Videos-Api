aee87694938a75704df3bfc084170eb5aeac344a
Alex Castelo <alexcastelo437@gmail.com>
2022-11-26 01:09:49 -0300

    feat: defined user service

---
+++ src/app/service/UserService.js
@@ -0,0 +1,31 @@
+const UserRepository = require("../repository/UserRepository")
+
+class UserService {
+    async create(payload) {
+        const result = await UserRepository.create(payload)
+        return result
+    }
+
+    async list(payload) {
+        const result = await UserRepository.list(payload)
+        return result
+    }
+
+    async getById(id) {
+        const result = await UserRepository.getById(id)
+        return result
+    }
+
+    async update(id, payload) {
+        const result = await UserRepository.update(id, payload)
+        return result
+    }
+
+    async delete(id) {
+        const result = await UserRepository.delete(id)
+        
+    }
+
+}
+
+module.exports = new UserService()
