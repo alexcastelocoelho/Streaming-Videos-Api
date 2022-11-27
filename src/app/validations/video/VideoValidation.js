04a4f03bd22923ad588cf9f127ae814bb1ba2e0b
Alex Castelo <alexcastelo437@gmail.com>
2022-11-27 15:10:28 -0300

    feat: created validation for videos

---
+++ src/app/validations/video/VideoValidation.js
@@ -0,0 +1,28 @@
+const joi = require("joi").extend(require("@joi/date"))
+
+module.exports = async (req, res, next) => {
+
+    try {
+        const validVideo = joi.object({
+          title: joi.string().max(45).required(),
+          description: joi.string().max(90).required(),
+          author: joi.string().min(3).max(20).required(),
+          genre: joi.string().min(4).required(),
+          tags: joi.array().min(1).message("tags cannot be empty").required()
+        })
+
+        const {error} = await validVideo.validate(req.body, {
+            abortEarly: false
+        }) 
+        if (error) throw error
+
+        return next()
+    } catch (error) {
+        return res.status(400).json({
+          errors: error.details.map((alert) => ({
+            description: alert.message,
+            name: alert.path.join('.')
+          }))
+        });
+      }
+}
