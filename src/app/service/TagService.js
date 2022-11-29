74974c2c6cdfa10b89f84b77aa0db0e9ca81b237
Alex Castelo <alexcastelo437@gmail.com>
2022-11-28 21:33:17 -0300

    feat: created tag service

---
+++ src/app/service/TagService.js
@@ -0,0 +1,34 @@
+const TagRepository = require("../repository/TagRepository")
+const VideoRepository = require("../repository/VideoRepository")
+
+class TagService {
+    async create(payload) {                   
+        const result = await TagRepository.create(payload)
+        return result      
+       
+    }
+
+    async list(payload) {
+        const result = await TagRepository.list(payload)
+        return result
+    }
+
+    async getVideoTag(title_tag) {                
+            const result = await TagRepository.getVideoTag(title_tag)
+            return result                 
+        
+    }
+
+    async update(id, payload) {
+        const result = await TagRepository.update(id, payload)
+        return result
+    }
+
+    async delete(id) {
+        const result = await TagRepository.delete(id)
+        
+    }
+
+}
+
+module.exports = new TagService()
