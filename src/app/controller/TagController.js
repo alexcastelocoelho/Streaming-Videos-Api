bcf3307c9f0867c9dc8be4bbd340f87d1bb5ffe0
Alex Castelo <alexcastelo437@gmail.com>
2022-11-28 21:33:35 -0300

    feat: created tag controller

---
+++ src/app/controller/TagController.js
@@ -0,0 +1,51 @@
+const TagService = require("../service/TagService");
+
+class TagController {
+    async create(req, res ) {
+        try {
+            const result = await TagService.create(req.body)
+            return res.status(201).json(result)
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
+
+    async list(req, res) {
+        try {
+            const result = await TagService.list(req.query)
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json(error) 
+        }
+    }
+
+    async getVideoTag(req, res) {
+        try {
+            const result = await TagService.getVideoTag(req.params.title_tag) 
+            return res.status(200).json(result)
+        } catch (error) {
+            // return res.status(400).json(error)
+            console.log(error)
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const result = await TagService.update(req.params.id, req.body)
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json(error);
+          }
+    }
+
+    async delete(req, res) {
+        try {
+            const result = await TagService.delete(req.params.id)
+            return res.status(204).json(result)
+        } catch (error) {
+            return res.status(400).json(error);
+          }
+    }
+}
+
+module.exports = new TagController()
