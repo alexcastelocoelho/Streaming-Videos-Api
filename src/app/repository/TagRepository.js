268cefb0b4ce9894b5206b0cfa14701c4cb10e51
Alex Castelo <alexcastelo437@gmail.com>
2022-11-28 21:32:57 -0300

    feat: created tag repository

---
+++ src/app/repository/TagRepository.js
@@ -0,0 +1,31 @@
+const TagSchema = require("../schema/TagSchema")
+
+class TagRepository {
+    async create(payload) {
+        const response = await TagSchema.create(payload)
+        return response        
+    }
+
+    async list(payload) {
+        const response = await TagSchema.find(payload)
+        return response
+    }
+    
+    async getVideoTag(title_tag) {        
+        
+        const response = await TagSchema.findOne(title_tag)
+        return response
+    }
+    
+    async update(id, payload) {
+        const response = await TagSchema.findByIdAndUpdate(id, payload)
+        return response
+    }
+    
+    async delete(id) {
+        const response = await TagSchema.findByIdAndDelete(id)
+        return response
+    }    
+}
+
+module.exports = new TagRepository()
