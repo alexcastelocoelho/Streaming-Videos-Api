4125037b1df0ed6040a291fe158549a887e1773e
Alex Castelo <alexcastelo437@gmail.com>
2022-11-28 21:35:27 -0300

    feat: added tag routes in main routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const {Router} = require("express")
 const User = require("./UserRouter")
 const Video = require("./VideoRouter")
+const Tag = require("./TagRouter")
 module.exports = (server) => {
     server.use((req, res, next) => {
         User(server, new Router())
         Video(server, new Router())
+        Tag(server, new Router())
 
         next();
     })
