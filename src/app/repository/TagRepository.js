7323dca47e98a4f2131eb034a090989d334fedaf
Alex Castelo <alexcastelo437@gmail.com>
2022-12-01 14:38:16 -0300

    feat: populating video field

--- src/app/repository/TagRepository.js
+++ src/app/repository/TagRepository.js
@@ -11,12 +11,12 @@ class TagRepository {
         return response
     }
     
-    async getVideoTag(title_tag) {        
+    async getVideoTag(title_tag) {    
         
-        const response = await TagSchema.findOne(title_tag)
+        const response = await TagSchema.findOne({title_tag: title_tag}).populate("video")
         return response
     }
-    
+
     async update(id, payload) {
         const response = await TagSchema.findByIdAndUpdate(id, payload)
         return response
